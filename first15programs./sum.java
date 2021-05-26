48e102aac7e5e4c851a0c0f0702340bd770abaab
Swathisaseendran <79155802+Swathisaseendran@users.noreply.github.com>
2021-05-26 11:59:40 +0530

    Add files via upload

---
+++ first15programs./sum.java
@@ -0,0 +1,9 @@
+public class sum
+{
+        public static void main(String args[])
+        {
+           int a=2,b=4,sum=0;
+           sum=a+b; 
+           System.out.println("sum of "+a+" and "+b+":"+sum);
+        }
+}
